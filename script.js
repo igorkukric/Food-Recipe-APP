d4a905cc5ac7ec8756f0286a79982c57ffe0a236
igorkukric <you@example.com>
2023-09-12 23:59:55 +0200

    Function to fetch meals by ingredient

--- script.js
+++ script.js
@@ -14,13 +14,26 @@ searchButton.addEventListener("click", async () => {
   }
 });
 
-mealList.addEventListener('click', async (e)=>{
-    const card = e.target.closest('meal-item')
-    if(card) {
-        const mealId = card.dataset.id
-        const meal = await getMealDetails(mealId)
-        if(meal) {
-            showMealDetailsPopup(meal)
-        }
+mealList.addEventListener("click", async (e) => {
+  const card = e.target.closest("meal-item");
+  if (card) {
+    const mealId = card.dataset.id;
+    const meal = await getMealDetails(mealId);
+    if (meal) {
+      showMealDetailsPopup(meal);
     }
-})
+  }
+});
+
+// Function to fetch meals by ingredient
+async function searchMealsByIngredient(ingredient) {
+  try {
+    const response = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`
+    );
+    const data = await response.json();
+    return data.meals;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
